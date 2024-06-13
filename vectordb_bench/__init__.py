f185bb24d5d7050c64fd5decaf1b435f5ae6d58c
melodyshu <1321497435@qq.com>
2024-06-13 15:33:27 +0800

    1.修改并发数 2.修改测试时长

--- vectordb_bench/__init__.py
+++ vectordb_bench/__init__.py
@@ -19,7 +19,7 @@ class config:
 
     DROP_OLD = env.bool("DROP_OLD", True)
     USE_SHUFFLED_DATA = env.bool("USE_SHUFFLED_DATA", True)
-    NUM_CONCURRENCY = [1, 5, 10, 15, 20, 25, 30, 35]
+    NUM_CONCURRENCY = [1, 5, 10, 20, 30, 40, 50, 60, 70]
 
     RESULTS_LOCAL_DIR = pathlib.Path(__file__).parent.joinpath("results")
 
